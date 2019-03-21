4bcc3cfb05aaa1861d3e371caf0c8e5f001439a0
Jack Pertschuk <jp954@cornell.edu>
2019-03-21 17:19:17 -0400

    asd

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ $(document).ready(function() {
 })
 
 
+function runClock() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById("date").innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(runClock, 500);
+}

34664d73c5738ab759bcf63d5033df9b7a262d47
taoge <tg367@cornell.edu>
2019-11-05 19:23:33 -0500

    stesh

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	getTimetable();
+    getTimetable();
+    //runClock();
 	// console.log(data)
 	// data[0].RouteDirections;
 	// for (let route of data[0].RouteDirections) {
@@ -13,9 +14,16 @@ function runClock() {
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
+    year=today.getFullYear();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById("date").innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(runClock, 500);
+    
+    $("#clock").html(h + ":" + m + ":" + s);
+    
+    $("#date").html(year+'/'+today.getMonth()+'/'+today.getDate());
+    var t = setTimeout(runClock, 1000);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
 }

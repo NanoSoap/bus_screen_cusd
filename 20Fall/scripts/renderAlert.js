ef0ca6ab2349756ada154715121157fef8adde5c
taoge <tg367@cornell.edu>
2020-10-13 00:46:55 -0400

    alert scroll

---
+++ 20Fall/scripts/renderAlert.js
@@ -0,0 +1,33 @@
+const width=screen.width
+function getAlert() {
+    var token = 'Bearer e5159b89-86c1-3cca-8412-59de037c674b';
+    setTimeout(getAlert,10000000)
+    return $.ajax({
+      url: 'https://gateway.api.cloud.wso2.com:443/t/mystop/tcat/v1/rest/PublicMessages/GetCurrentMessages',
+      type: 'GET',
+      dataType: 'json',
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader("Authorization", token);
+      },
+      success: bindAlerts,
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        alert(errorThrown);
+      }
+      })
+  }
+  function bindAlerts(alerts,textStatus,XMLHttpRequest) {
+      console.log('alerts :>> ', alerts);
+      let message=''
+      for (let index = 0; index < alerts.length; index++) {
+        const element = alerts[index]['Message'];
+        message+='▶︎'
+        message+=element
+      }
+      $('#alert_txt').text(message).css("animation-duration", message.length*0.2+width*0.003+'s')
+
+      // alerts.forEach(alert => {
+      //       let message=alert['Message']
+            
+      // });
+  }
+  getAlert()
--- 20Fall/scripts/renderData.js
+++ 20Fall/scripts/renderData.js
@@ -173,30 +173,6 @@ function render2(msg) {
 //     success: render3
 //   });
 // }
-function getAlert() {
-  var token = 'Bearer e5159b89-86c1-3cca-8412-59de037c674b';
-  return $.ajax({
-    url: 'https://gateway.api.cloud.wso2.com:443/t/mystop/tcat/v1/rest/PublicMessages/GetCurrentMessages',
-    type: 'GET',
-    dataType: 'json',
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader("Authorization", token);
-    },
-    success: function(msg) {
-      var alerts='';
-      for (let alert of msg) {
-        
-           alerts += alert.Message;
-           alerts+=" ||";
-          
-        
-      }
-      $('#scrollalert').text(alerts);
-    },
-    error: function(XMLHttpRequest, textStatus, errorThrown) {
-      alert(errorThrown);
-    }
-    })
-}
+
 
 
